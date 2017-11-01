3c8677116bfbde9636e19a1f6173ec97acd3e02a
Griffin Maxwell <griffin@maxwell-family.net>
2017-11-01 14:26:20 -0400

    disposable omp pragma in convergence loop

--- Source/maxwell_griffin_disposable.c
+++ Source/maxwell_griffin_disposable.c
@@ -16,20 +16,25 @@
 #define MS_PER_SEC (1000)
 #define NS_PER_MS (1000000)
 
+// Objects
 static Map_Box_t mapIdToBox;
 static Map_Double_t mapIdToUpdatedTemperature;
 static FormattedReader_Box_t boxReader;
 static DsvUpdater_BoxTemperature_t dsvUpdater;
 
+// User args and OpenMP actual threads
 static double affectRate;
 static double epsilon;
-static int numThreads;
+static int numRequestedThreads;
+static int numActualThreads;
 
+// Box dimensions
 static uint32_t numBoxes;
 static uint32_t numGridRows;
 static uint32_t numGridCols;
 static uint32_t numIterations;
 
+// Timing variables
 static clock_t clockStart;
 static clock_t clockEnd;
 static time_t timeStart;
@@ -37,6 +42,7 @@ static time_t timeEnd;
 static struct timespec rtcStart;
 static struct timespec rtcEnd;
 
+// Min Max Temperatures
 static double minTemperature;
 static double maxTemperature;
 
@@ -58,11 +64,13 @@ static void DisplayStats()
 {
    printf("\n");
    printf("********************************************************************************\n");
+   printf("Using \"disposable\" threading with OpenMP:\n");
    printf("temperature dissipation converged in %d iterations\n", numIterations);
-   printf("    with number of (disposable) pthreads = %d\n", numThreads);
-   printf("    with max DSV = %lf and min DSV = %lf\n", maxTemperature, minTemperature);
-   printf("    AFFECT_RATE = %lf;\tEPSILON = %lf\n", affectRate, epsilon);
-   printf("    Num boxes = %d;\tNum rows = %d;\tNum columns = %d\n", numBoxes, numGridRows, numGridCols);
+   printf(" -  with requested number of threads = %d\n", numRequestedThreads);
+   printf(" -  with actual number of threads = %d\n", numActualThreads);
+   printf(" -  with max DSV = %lf and min DSV = %lf\n", maxTemperature, minTemperature);
+   printf(" -  AFFECT_RATE = %lf;\tEPSILON = %lf\n", affectRate, epsilon);
+   printf(" -  Num boxes = %d;\tNum rows = %d;\tNum columns = %d\n", numBoxes, numGridRows, numGridCols);
    printf("\n");
 
    printf("elaspsed convergence loop time:\n");
@@ -78,8 +86,7 @@ static void DisplayStats()
 
 static void ReadInputGrid()
 {
-   uint32_t i;
-   for(i = 0; i < numBoxes; i++)
+   for(int i = 0; i < numBoxes; i++)
    {
       int id;
       fscanf(stdin, "%d", &id);
@@ -91,17 +98,17 @@ static void ReadInputGrid()
    }
 }
 
-static void * ThreadSafeCalculateUpdatedBoxTemperatures(void *args)
+static void CalculateBlockOfBoxTemperatures(int threadId, int numThreads)
 {
-   REINTERPRET(threadId, args, int *);
-
-	// block distribution
+	// block distribution parameters
 	int boxesPerThread = numBoxes / numThreads;
-	int start = (*threadId) * boxesPerThread;
-	int end = (*threadId == numThreads - 1) ? numBoxes : start + boxesPerThread;	// gives leftover boxes to the last thread
+	int start = threadId * boxesPerThread;
+	int end =  // gives leftover boxes to the last thread
+      (threadId == numRequestedThreads - 1)
+      ? numBoxes
+      : start + boxesPerThread;
 
-	uint32_t i;
-   for(i = start; i < end; i++)
+   for(int i = start; i < end; i++)
    {
       Box_t *box = Map_Find(&mapIdToBox.interface, i);
       if(NULL != box)
@@ -111,15 +118,11 @@ static void * ThreadSafeCalculateUpdatedBoxTemperatures(void *args)
 			Map_Add(&mapIdToUpdatedTemperature.interface, i, &updatedTemperature);
       }
    }
-
-   free(threadId);
-   pthread_exit(NULL);
 }
 
-static void CommitUpdatedBoxTemperaturesAndFindMinMax()
+static void CommitBoxTemperaturesAndFindMinMax()
 {
-	uint32_t i;
-   for(i = 0; i < numBoxes; i++)
+   for(int i = 0; i < numBoxes; i++)
    {
       Box_t *box = Map_Find(&mapIdToBox.interface, i);
       if(NULL != box)
@@ -145,14 +148,14 @@ int main(int argc, char *argv[])
    // Parse command line args
    sscanf(argv[1], "%lf", &affectRate);
    sscanf(argv[2], "%lf", &epsilon);
-   sscanf(argv[3], "%d", &numThreads);
+   sscanf(argv[3], "%d", &numRequestedThreads);
 
    // Read first line of stdin for number of boxes and grid dimensions
    fscanf(stdin, "%d %d %d", &numBoxes, &numGridRows, &numGridCols);
 
    // Initialize objects
-   Map_Box_Init(&mapIdToBox, (uint32_t)numBoxes);
-   Map_Double_Init(&mapIdToUpdatedTemperature, (uint32_t)numBoxes);
+   Map_Box_Init(&mapIdToBox, numBoxes);
+   Map_Double_Init(&mapIdToUpdatedTemperature, numBoxes);
    FormattedReader_Box_Init(&boxReader, stdin);
    DsvUpdater_BoxTemperature_Init(&dsvUpdater, &mapIdToBox, affectRate);
 
@@ -164,13 +167,13 @@ int main(int argc, char *argv[])
    bool hasConverged = false;
 	for(numIterations = 0; !hasConverged; numIterations++)
    {
-      // Parallelize this region (request # threads)
-      // {
-      //    Store actual number of threads
-      //    CalculateUpdatedBoxTemperatures
-      // }
+      #pragma omp parallel num_threads(numRequestedThreads)
+      {
+         numActualThreads = omp_get_num_threads();
+         CalculateBlockOfBoxTemperatures(omp_get_thread_num(), numActualThreads);
+      }
 
-      CommitUpdatedBoxTemperaturesAndFindMinMax();
+      CommitBoxTemperaturesAndFindMinMax();
       hasConverged = HAS_CONVERGED(maxTemperature, minTemperature, epsilon);
    }
 
